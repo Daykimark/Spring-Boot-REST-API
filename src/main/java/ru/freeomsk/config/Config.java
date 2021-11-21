612cbf62f6489a5d8e74d73ead7d48da8a99bc69
freeomsk <freeomsk@yandex.ru>
2021-11-21 11:38:56 +0600

    Initial commit

---
+++ src/main/java/ru/freeomsk/config/Config.java
@@ -0,0 +1,10 @@
+package ru.freeomsk.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("ru.freeomsk")
+public class Config {
+
+}
